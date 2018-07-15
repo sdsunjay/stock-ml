99995d74bddc2ef119ecf576b0ef2bba97f1045f
Sunjay <sdsunjay73@yahoo.com>
2018-07-14 21:19:06 -0700

    Iterate through all classifiers

--- rank_stocks.py
+++ rank_stocks.py
@@ -26,25 +26,38 @@ def fetch_data_from_file(filename):
     return data
 
 def main():
-    clf = joblib.load('Logistic Regression.pkl')
-    if os.path.isdir(parse_data.CLEAN_TEST_DATA_DIR) == False:
-          print(parse_data.CLEAN_TEST_DATA_DIR + ' not found')
-          return
-    data = []
-    for filename in os.listdir(parse_data.CLEAN_TEST_DATA_DIR):
-        path = os.path.join(parse_data.CLEAN_TEST_DATA_DIR, filename)
-        if os.path.isfile(path) == False:
-            print('Error: Skipping ' + path)
-            continue
-        data = fetch_data_from_file(path)
-        # Pulled off label.
-        x = [[float(val) for val in point[0:-1]] for point in data]
-        y = [int(point[-1]) for point in data]
-        predictions = clf.predict(x)
 
-        if 1 in predictions:
-            print(filename)
-            print(str(predictions))
+    names = ["Logistic Regression", "Nearest Neighbors", "RBF SVM", "RBF SVM .1 Gamma", "Decision Tree", "Random Forest", "Neural Net", "AdaBoost", "Naive Bayes", "QDA"]
+    for name in names:
+        print('Classifier: ' + name)
+        classifier_path = 'classifiers/' + name + '.pkl'
+        if os.path.isfile(classifier_path) == False:
+            print('Error: Skipping ' + classifier_path)
+            continue
+        clf = joblib.load(classifier_path)
+        if os.path.isdir(parse_data.CLEAN_TEST_DATA_DIR) == False:
+              print(parse_data.CLEAN_TEST_DATA_DIR + ' not found')
+              return
+        data = []
+        for filename in os.listdir(parse_data.CLEAN_TEST_DATA_DIR):
+            path = os.path.join(parse_data.CLEAN_TEST_DATA_DIR, filename)
+            if os.path.isfile(path) == False:
+                print('Error: Skipping ' + path)
+                continue
+            data = fetch_data_from_file(path)
+            # Pulled off label.
+            x = [[float(val) for val in point[0:-1]] for point in data]
+            y = [int(point[-1]) for point in data]
+            predictions = clf.predict(x)
+            dates = ['2018-06-01', '2018-06-04', '2018-06-05', '2018-06-06',
+            '2018-06-07', '2018-06-08', '2018-06-11', '2018-06-12',
+            '2018-06-13', '2018-06-14', '2018-06-15', '2018-06-18',
+            '2018-06-19', '2018-06-20', '2018-06-21', '2018-06-22',
+            '2018-06-25']
+            if 1 in predictions:
+                print(filename)
+                print(str(dates))
+                print(str(predictions))
    # Plan of attack:
    # read each stock file, get all the features
    # run the classifier on each stock file, given 2 weeks of data to predict
