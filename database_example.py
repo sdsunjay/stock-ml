3cbd11e59d357ff49f3b2913c6bfd5e52ef5f066
Sunjay <sdsunjay73@yahoo.com>
2018-07-16 16:32:25 -0700

    WIP - bulk insert

--- database_example.py
+++ database_example.py
@@ -2,42 +2,66 @@ import pprint
 import pymongo as pym
 from pymongo import MongoClient
 import datetime
+import os
+RAW_DATA_PATH = os.path.join('tmp')
 # Stock symbol, Date, Open price, High Price, Low Price, Closing Price, Volume of shares
 # AAON,20170103,33.45,33.7,32.95,33.35,137600
 # FB,20170103,116.03,117.84,115.51,116.86,20663900
 #
 
-def insert(db):
-    symbol = 'FB'
-    open_price = 116.03
-    high_price = 117.84
-    low_price = 115.51
-    close_price = 116.86
-    volume = 20663900
-    date = datetime.datetime.strptime("20170103", "%Y%m%d")
+# stocks.create_index([("symbol", pym.DESCENDING), ("symbol", pym.ASCENDING)], unique=True)
+# stocks.createIndex( { symbol: symbol, date: date }, {unique: true} )
+
+
+def store_in_db(db, data):
+    stocks = []
+    stock = {}
+    for line in data:
+
+        symbol = line[0]
+        date = datetime.datetime.strptime(line[1], "%Y%m%d")
+        open_price = line[2]
+        high_price = line[3]
+        low_price = line[4]
+        close_price = line[5]
+        volume = line[6]
+        stock['date'] = date
+        stock['symbol']  = symbol
+        stock['open_price'] = open_price
+        stock['high_price'] = high_price
+        stock['close_price'] = close_price
+        stock['volume'] = volume
+        stocks.append(stock)
 
     collection = db['stocks']
-    stock = {}
-    stock['date'] = date
-    stock['symbol']  = symbol
-    stock['open_price'] = open_price
-    stock['high_price'] = high_price
-    stock['close_price'] = close_price
-    stock['volume'] = volume
-    stocks = db.stocks
-    collection.insert(stock)
-    # stocks.create_index([("symbol", pym.DESCENDING), ("symbol", pym.ASCENDING)], unique=True)
-    # stocks.createIndex( { symbol: symbol, date: date }, {unique: true} )
+    collection.insert(stocks)
+
+def fetch_data(db):
+    ''' Read stock data from all CSV files in RAW_DATA_PATH'''
+    data = []
+    if os.path.isdir(RAW_DATA_PATH) == False:
+        print(RAW_DATA_PATH + ' not found')
+        return data
+    for filename in os.listdir(RAW_DATA_PATH):
+        path = os.path.join(RAW_DATA_PATH, filename)
+
+        if os.path.isfile(path) == False:
+            continue
+
+        with open(path) as f:
+            for line in f:
+                data.append(line.strip().split(','))
+        store_in_db(db, data)
+    return data
 
 
 def main():
     # client = MongoClient()
     client = MongoClient('localhost', 27017)
     db = client['test-database']
+    fetch_data(db)
     stocks = db.stocks
     # db.stocks.remove({})
-    # insert(db)
-    # insert(db)
 
     # pprint.pprint(stocks.find({"date": "2017-01-04 00:00:00"}))
     # find all documents
@@ -48,6 +72,7 @@ def main():
     # display documents from collection
     for record in results:
         # print out the document
-        print(record['symbol'] + ',',record['date'])
+        print(record['symbol'] + ',' + str(record['date']) + ',' +
+        str(record['close_price']) )
 if __name__ == '__main__':
     main()
