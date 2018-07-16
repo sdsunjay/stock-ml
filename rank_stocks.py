926d83b28394d8a5914863ae5e4d74376b434cf2
Sunjay <sdsunjay73@yahoo.com>
2018-07-15 17:38:26 -0700

    Moved code for reading test files into function

--- rank_stocks.py
+++ rank_stocks.py
@@ -25,9 +25,30 @@ def fetch_data_from_file(filename):
             data.append(line.split("\t"))
     return data
 
+def read_test_files(clf):
+    ''' Read test files in test dir and make predictions using classifier (clf) '''
+    data = []
+    for filename in os.listdir(parse_data.CLEAN_TEST_DATA_DIR):
+        path = os.path.join(parse_data.CLEAN_TEST_DATA_DIR, filename)
+        if os.path.isfile(path) == False:
+            print('Error: Skipping ' + path)
+            continue
+        data = fetch_data_from_file(path)
+        # Pulled off label.
+        x = [[float(val) for val in point[0:-1]] for point in data]
+        y = [int(point[-1]) for point in data]
+        predictions = clf.predict(x)
+        dates = ['2018-05-01', '2018-05-02', '2018-05-03', '2018-05-04', '2018-05-07', '2018-05-08', '2018-05-09','2018-05-10', '2018-05-11', '2018-05-14', '2018-05-15', '2018-05-16', '2018-05-17', '2018-05-18', '2018-05-21', '2018-05-22', '2018-05-23', '2018-05-24', '2018-05-25', '2018-05-29', '2018-05-30', '2018-05-31', '2018-06-01']
+        if 1 in predictions:
+            if 1 in y:
+                print(filename)
+        #    print(str(dates))
+                print(y)
+                print(str(predictions))
+
 def main():
 
-    names = ["Logistic Regression", "Nearest Neighbors", "RBF SVM", "RBF SVM .1 Gamma", "Decision Tree", "Random Forest", "Neural Net", "AdaBoost", "Naive Bayes", "QDA"]
+    names = ["Logistic Regression", "Nearest Neighbors", "Decision Tree", "Random Forest", "Neural Net", "AdaBoost", "Naive Bayes", "QDA"]
     for name in names:
         print('Classifier: ' + name)
         classifier_path = 'classifiers/' + name + '.pkl'
@@ -38,26 +59,7 @@ def main():
         if os.path.isdir(parse_data.CLEAN_TEST_DATA_DIR) == False:
               print(parse_data.CLEAN_TEST_DATA_DIR + ' not found')
               return
-        data = []
-        for filename in os.listdir(parse_data.CLEAN_TEST_DATA_DIR):
-            path = os.path.join(parse_data.CLEAN_TEST_DATA_DIR, filename)
-            if os.path.isfile(path) == False:
-                print('Error: Skipping ' + path)
-                continue
-            data = fetch_data_from_file(path)
-            # Pulled off label.
-            x = [[float(val) for val in point[0:-1]] for point in data]
-            y = [int(point[-1]) for point in data]
-            predictions = clf.predict(x)
-            dates = ['2018-06-01', '2018-06-04', '2018-06-05', '2018-06-06',
-            '2018-06-07', '2018-06-08', '2018-06-11', '2018-06-12',
-            '2018-06-13', '2018-06-14', '2018-06-15', '2018-06-18',
-            '2018-06-19', '2018-06-20', '2018-06-21', '2018-06-22',
-            '2018-06-25']
-            if 1 in predictions:
-                print(filename)
-                print(str(dates))
-                print(str(predictions))
+        read_test_files(clf)
    # Plan of attack:
    # read each stock file, get all the features
    # run the classifier on each stock file, given 2 weeks of data to predict
